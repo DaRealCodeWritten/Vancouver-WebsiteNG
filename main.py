30fe5f8266dcd6f514b4467fec35e8dc06f8b72e
DaRealCodeWritten <sswiftgamer440@gmail.com>
2022-06-06 02:14:13 -0700

    2 new endpoints, fix login and DNS, add icon, did not commit 5000 HTML files

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import ssl
 from utils.configreader import config
-from requests import post
+from requests import post, get
 from mysql.connector import connect
 from flask import request, redirect, Flask, render_template, Response, abort
 from flask_login import current_user, login_user, logout_user, UserMixin, AnonymousUserMixin, LoginManager
@@ -28,6 +29,8 @@ class AnonymousUser(AnonymousUserMixin):
         return False
 
 
+context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+context.load_cert_chain("cert.pem", "key.key")
 app_config = config()
 app = Flask(__name__)
 app.secret_key = app_config["FLASK_CLIENT_SECRET"]
@@ -45,15 +48,32 @@ database = connect(
 
 @manager.user_loader
 def load_user(user_id: int):
-    return active_users.get(user_id)
+    return active_users.get(int(user_id))
 
 
 @app.route("/")
 def index():
     if current_user.is_authenticated is False:
-        return render_template("index.html", welcome="")
+        return render_template("index.html")
     else:
-        return render_template("index.html", welcome=f"Welcome, {current_user.name}")
+        return render_template("index_active.html", welcome=f"{current_user.name}")
+
+
+@app.route("/logout")
+def logout():
+    if current_user.is_authenticated:
+        logout_user(current_user)
+        active_users.pop(current_user.get_id())
+        return redirect("/")
+    else:
+        return redirect("/")
+
+
+@app.route("/profile")
+def profile():
+    if not current_user.is_authenticated:
+        return redirect("https://auth-dev.vatsim.net/oauth/authorize?client_id=383&redirect_uri=https%3A%2F%2Fczvr-bot.xyz%2Fcallback%2Fvatsim&response_type=code&scope=full_name+vatsim_details")
+    return render_template("profile.html", name=current_user.name, )
 
 
 @app.route("/callback/vatsim")
@@ -65,9 +85,9 @@ def login():
         rqst = {
             "code": code,
             "client_id": app_config["VATSIM_CLIENT_ID"],
-            "client_secret": app_config["VATSIM_CLIENT_ID"],
+            "client_secret": app_config["VATSIM_CLIENT_SECRET"],
             "grant_type": "authorization_code",
-            "redirect_uri": "http://czvr-bot.xyz:5000/callback/vatsim"
+            "redirect_uri": "https://czvr-bot.xyz/callback/vatsim"
         }
         resp = post("https://auth-dev.vatsim.net/oauth/token", data=rqst)
         token = resp.json().get("access_token")
@@ -77,11 +97,11 @@ def login():
             "Authorization": f"Bearer {token}",
             "accept": "application/json"
         }
-        data = post("https://auth-dev.vatsim.net/api/user", headers=headers)
+        data = get("https://auth-dev.vatsim.net/api/user", headers=headers)
         jsdata = data.json()
         if jsdata.get("data") is None:
             return abort(Response("VATSIM API returned a malformed response", 400))
-        cid = jsdata["data"]["cid"]
+        cid = int(jsdata["data"]["cid"])
         rating = jsdata["data"]["vatsim"]["rating"]["id"]
         personal = jsdata["data"].get("personal")
         if personal is None:
@@ -90,7 +110,7 @@ def login():
             name = personal["name_first"]
         cursor = database.cursor()
         cursor.execute(
-            f"INSERT INTO {app_config['DATABASE_TABLE']} VALUES ({cid}, NULL, {rating}) ON DUPLICATE KEY UPDATE rating = {rating}"
+            f"INSERT INTO {app_config['DATABASE_TABLE']} VALUES ({cid}, NULL, {rating}, NULL) ON DUPLICATE KEY UPDATE rating = {rating}"
         )
         cursor.close()
         database.commit()
@@ -98,9 +118,9 @@ def login():
         active_users[cid] = user
         login_user(user, True)
     except Exception as e:
-        pass
-    return redirect("localhost:5000/")
+        return str(e)
+    return redirect("https://czvr-bot.xyz/")
 
 
 if __name__ == "__main__":
-    app.run("0.0.0.0", 5000)
+    app.run("0.0.0.0", 443, ssl_context=context)
